e7601e7cf19f47ecd76d1ab68a6df88225917ac1
Murilo Pina <murilo.mpina@gmail.com>
2025-09-03 11:23:54 -0300

    Adicionando pastas er arquivos

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../css/header.css'
+import '../css/global.css'
+
+function Header() {
+    return(
+        <div>
+            <h1>Calculadora IMC</h1>
+        </div>
+    )
+}
+export default Header
---
+++ src/components/TabelaIMC.jsx
@@ -0,0 +1,40 @@
+const TabelaIMC=(resultado)=>{
+    if(resultado<18){
+        return (
+            <>
+            <td>Abaixo do peso</td>
+            <td>Menor que 18</td>
+            </>
+        );
+    }else if (resultado >18 && resultado <25){
+        return(
+            <>
+            <td>Peso Normal</td>
+            <td>18 - 24.9</td>
+            </>
+        );
+    }else if(resultado>24 && resultado<30){
+        return(
+            <>
+            <td>Sobre Peso</td>
+            <td>25 - 29.9</td>
+            </>
+        );
+    }else if (resultado > 29 && resultado <35){
+        return(
+            <>
+            <td>Obesidade Grau-1</td>
+            <td>30 - 34.9</td>
+            </>
+        );
+    }else{
+        return(
+            <>
+            <td>Obesidade Mórbida</td>
+            <td>Maior que 40</td>
+            </>
+        );
+    }
+}
+
+export default TabelaIMC
