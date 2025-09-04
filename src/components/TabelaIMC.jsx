028c6a54639978ff8dd3b4aaf2e8f2694d9fef2d
Murilo Pina <murilo.mpina@gmail.com>
2025-09-04 16:44:12 -0300

    Finalizando estilização da tabela

--- src/components/TabelaIMC.jsx
+++ src/components/TabelaIMC.jsx
@@ -2,36 +2,36 @@ const TabelaIMC=(resultado)=>{
     if(resultado<18){
         return (
             <>
-            <td>Abaixo do peso</td>
-            <td>Menor que 18</td>
+            <td className="baixoPeso">Abaixo do peso</td>
+            <td>&lt; 18</td>
             </>
         );
     }else if (resultado >18 && resultado <25){
         return(
             <>
-            <td>Peso Normal</td>
+            <td className="pesoNormal">Peso Normal</td>
             <td>18 - 24.9</td>
             </>
         );
     }else if(resultado>24 && resultado<30){
         return(
             <>
-            <td>Sobre Peso</td>
+            <td className="sobrePeso">Sobre Peso</td>
             <td>25 - 29.9</td>
             </>
         );
     }else if (resultado > 29 && resultado <35){
         return(
             <>
-            <td>Obesidade Grau-1</td>
+            <td className="obesidade1">Obesidade Grau-1</td>
             <td>30 - 34.9</td>
             </>
         );
     }else{
         return(
             <>
-            <td>Obesidade Mórbida</td>
-            <td>Maior que 40</td>
+            <td className="obesidadeMorbida">Obesidade Mórbida</td>
+            <td>&gt; 40</td>
             </>
         );
     }
